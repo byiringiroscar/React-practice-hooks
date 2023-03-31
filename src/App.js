21925ab19449a39e9f5cfe7dc7df2306b56b3554
byiringiroscar <byiringoroscar@gmail.com>
2023-03-31 08:35:06 +0200

    Add full description of react hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const  [resourceType, setResourceType] = useState('posts')
+  const [item, setItem] = useState([]);
+  useEffect(() => {
+      fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItem(json));
+  }, [resourceType]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {item.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>
+      })}
+    </>
   );
 }
 
 export default App;
+// import { useState, useEffect } from 'react';
+// import './App.css';
+
+// function App() {
+//   const  [windowWidth, setwindowWidth] = useState(window.innerWidth)
+//   const handleResize = () => {
+//     setwindowWidth(window.innerWidth)
+//   }
+//   useEffect(() => {
+//       window.addEventListener('resize', handleResize)
+//   }, [])
+//   return (
+//     <div>{windowWidth}</div>
+//   );
+// }
+
+// export default App;
+
+
